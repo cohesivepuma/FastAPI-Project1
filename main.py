0b9f79716fcd8e4457f485d423da9773002b3061
cohesivepuma233 <546053862@qq.com>
2025-04-19 12:38:05 +0800

    chat model

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from starlette.middleware.sessions import SessionMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from settings import TORTOISE_ORM
 from models import User
+from starlette.websockets import WebSocket
+from fastapi import WebSocketDisconnect
 app = FastAPI()
 #1 引入静态模版文件
 register_tortoise(app,config=TORTOISE_ORM) #初始化数据库连接，生成模型与数据库之间的映射
@@ -95,6 +97,63 @@ async def index(request:Request):
     else:
         return RedirectResponse('/login')
 
+@router.get('/chat')
+async def chat(request:Request):
+    if request.session.get('username'):
+        return templates.TemplateResponse('chat.html',context={"request":request})
+    else:
+        return RedirectResponse('/login',status_code=302)
+
+#写ws来处理用户聊天
+@app.websocket('/ws')
+async def websocket_endpoint(websocket:WebSocket):
+    await websocket.accept() #等待用户连接
+    while True:
+        data = await websocket.receive_text() #接受用户信息
+        await websocket.send_text(data+'_nb') #发送用户信息
+
+
+# 多人聊天
+@router.get('/chat01')
+async def chat01(request:Request):
+    if request.session.get('username'):
+        return templates.TemplateResponse('chat01.html',context={"request":request,"username":request.session.get('username')})
+    else:
+        return RedirectResponse('/login',status_code=302)
+
+
+# 处理多人聊天，只要有一个人发送所有人都能收到
+class ConnectionManager:
+    def __init__(self):
+        #列表存放所有用户
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        # 发送广播消息
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+@app.websocket('/ws/{client_id}')
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{client_id} 说: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"{client_id} 离开了聊天室")
 app.include_router(router,prefix='',tags=['总路由'])
 #注意总路由要放在这里，不然无法识别
 if __name__ == '__main__':
